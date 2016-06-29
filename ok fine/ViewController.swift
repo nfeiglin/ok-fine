fe3c078efef2d21a67a3d2ed24b61049c67a523a
Nathan Feiglin <nathanf98@hotmail.com>
2016-06-30 00:45:30 +1000

    Initial Commit

---
+++ ok fine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ok fine
+//
+//  Created by Nathan Feiglin on 30/06/2016.
+//  Copyright © 2016 Nathan Feiglin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
