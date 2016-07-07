f4ed049ccf0f49a2a095397bda6bc1fa2f5d2410
Nathan Feiglin <nathan@Nathans-MacBook-Air.local>
2016-07-08 03:22:30 +1000

    Adds uid for jurisdiction data item for event tracking purposes

---
+++ ok fine/FormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormViewController.swift
+//  ok fine
+//
+//  Created by Nathan Feiglin on 8/07/2016.
+//  Copyright © 2016 Nathan Feiglin. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
